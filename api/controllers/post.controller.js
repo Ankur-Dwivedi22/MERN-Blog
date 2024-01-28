c7e56bf8c1081ddaaf9b9ebe87104e0a87ffb957
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2024-01-28 14:15:11 +0530

    complete post page functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -83,23 +83,24 @@ export const deletepost = async (req,res,next) => {
 };
 
 export const updatepost = async (req, res, next) => {
-    if(!req.user.isAdmin || req.params.userId !== req.user.id){
-        return next(errorHandler(403, 'You are not allowed to update this post'));
+    if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+      return next(errorHandler(403, 'You are not allowed to update this post'));
     }
-
-    try{
-        const updatePost = await Post.findByIdAndUpdate(
-            req.params.postId,
-            {
-                $set: {
-                    title: req.body.title,
-                    content: req.body.content,
-                    category: req.body.category,
-                    image: req.body.image,
-                }}, {new : true} )
-            res.status(200).json(updatePost);
+    try {
+      const updatedPost = await Post.findByIdAndUpdate(
+        req.params.postId,
+        {
+          $set: {
+            title: req.body.title,
+            content: req.body.content,
+            category: req.body.category,
+            image: req.body.image,
+          },
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedPost);
+    } catch (error) {
+      next(error);
     }
-    catch(error){
-        next(error);
-    }
-}
+  };
