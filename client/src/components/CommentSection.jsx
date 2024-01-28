96b2220ca77a2e1e7276d42ddd5c72bf9dab3590
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2024-01-29 00:35:46 +0530

    show the comments of a post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,13 +1,14 @@
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {Alert, Button, Textarea }from 'flowbite-react';
+import {Alert, Button, Textarea } from 'flowbite-react';
+import Comment from './Comment';
 
 export default function CommentSection({postId}) {
     const {currentUser} = useSelector(state => state.user);
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
-
+    const [comments , setComments] = useState([]);
     const handleSubmit = async (e) => {
         e.preventDefault();
         if(comment.length > 200 ){
@@ -25,6 +26,7 @@ export default function CommentSection({postId}) {
             if(res.ok) {
                 setComment('');
                 setCommentError(null);
+                setComments([data, ...comments]);
             }
         }
         catch(error){
@@ -32,6 +34,22 @@ export default function CommentSection({postId}) {
         }
 
     };
+
+    useEffect(() => {
+        const getComments = async () => {
+            try{
+                const res = await fetch(`/api/comment/getPostComments/${postId}`);
+                if(res.ok){
+                    const data = await res.json();
+                    setComments(data);
+                }
+            }
+            catch(error){
+                console.log(error);
+            }
+        };
+        getComments();
+    }, [postId]);
     return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         {currentUser ? 
@@ -50,23 +68,45 @@ export default function CommentSection({postId}) {
             </div>
         )}
         {currentUser && (
-            <form className='border border-teal-500 rounded-md p-3' onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className='border border-teal-500 rounded-md p-3' >
                 <Textarea 
                     placeholder='Add a comment..'
                     rows='3'
                     maxLength='200'
                     onChange={(e) => setComment(e.target.value)}
+                    value={comment}
                 />
                 <div className='flex justify-between items-center mt-5'>
                     <p className='text-gray-500 text-xs'>{200 - comment.length} characters remaining</p>
                     <Button outline gradientDuoTone='purpleToBlue' type='submit'>Submit</Button>
                 </div>
                 {commentError && 
-                    <Alert color='failure' className='mt-5'>
+                    (<Alert color='failure' className='mt-5'>
                 {commentError}
-            </Alert>}
+            </Alert>)}
             </form>
 
+        )}
+
+        {comments.length === 0 ? (
+            <p className='text-sm my-5'>No comments yet!</p>
+        ) : (
+            <>
+            <div className='text-sm my-5 flex items-center gap-1'>
+                <p>Comments</p>
+                <div className='border border-gray-400 py-1 px-2 rounded-sm'>
+                    <p>{comments.length}</p>
+                </div>
+            </div>
+            {
+                comments.map((comment) => (
+                    <Comment 
+                       key={comment._id}
+                       comment={comment} 
+                    />
+                ))
+            }
+            </>
         )}
     </div>
   )
